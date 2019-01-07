84f71ad1e33fe8794d0cf085c56d07a1f7cdf1ef
Marty Corbett <corbe054@umn.edu>
2019-01-06 20:58:05 -0600

    version 1.0.0

--- readme.md
+++ readme.md
@@ -1,8 +1,7 @@
 # roast-me-script
-This script uses PRAW (https://praw.readthedocs.io/en/latest/#) and the Reddit API to scrape the contents of r/roastme with the goal of finding the best roast comments.  It will then output the best comments to an html file for analysis.  The best comments are listed with a picture and description of the initial post.  To run the script you will need to enter in some variables:
+This script uses PRAW (https://praw.readthedocs.io/en/latest/#) and the Reddit API to scrape the contents of r/roastme with the goal of finding the best roast comments.  It will then output the best comments to an html file for analysis.  The best comments are listed..  To run the script you will need to enter one variables:
 
-Range:  Date Range of the posts to analyze.  This is inclusive.
 Comment Quality factor:  Number of upvotes the post and the comment has to have.
 
 ## Example Usage
-	python start.py --range 12/1/2018-12/8/2018 --cqf 1000
+	python start.py 1000
--- start.py
+++ start.py
@@ -5,70 +5,34 @@ import time
 import sys
 
 
-def roastme(cqf, start_date, end_date):
+def roastme(cqf):
 	start_time = timeit.default_timer()
 	reddit = praw.Reddit(client_id=client_id,
 	                     client_secret=client_secret,
 	                     user_agent=user_agent)
-
-	#print(reddit.read_only)  # Output: True
-
-	#total = 1
 	rank = []
 
-	api_time = timeit.default_timer()
-	print('begin')
-	print(cqf)
-	print(start_date)
-	print(end_date)
-
-	roast = reddit.subreddit('roastme').hot(limit=100)
-	print(timeit.default_timer() - api_time)
-
-	for submission in roast:
-		if submission.score >= 1000:
-			for_time = timeit.default_timer()
+	for submission in reddit.subreddit('roastme').hot(limit=1000):
+		if submission.score >= cqf:
 			submission.comments.replace_more(limit=0)
-			#top_level_comments = list(submission.comments)
-			print('top level comments: {}'.format(len(submission.comments)))
 			for comment in submission.comments:
-				#if comment
-				#body = comment.body
-				#if body != '[removed]' and comment.score >= 1000:
-				if comment.score >= 1000:
+				if comment.score >= cqf:
 					rank.append([comment.score, comment.body])
-					#print('{}: {}'.format(comment.score, body))
-					#total += 1
-
-		print(timeit.default_timer() - for_time)
-	    #print('{}: {}'.format(index + 1, submission.title))
 
 	rank = sorted(rank)
 	rank.reverse()
 
 	datetime = time.strftime("%Y%m%d%H%M%S")
 
-	file = open('{}-{}-{}-{}.txt'.format(filepath, cqf, start_date.replace('/', '-'), end_date.replace('/', '-')), 'w', encoding="utf-8")
+	file = open('{}-{}-{}.txt'.format(filepath, cqf, datetime), 'w', encoding="utf-8")
 
 	for index, r in enumerate(rank):
-		#print('{}: {}'.format(*r[0], *r[1]))
-		#print(*r)
 		file.write('{}: {}\n'.format(index, r[1]))
 
 	print('Elapsed Time: {} seconds'.format(timeit.default_timer() - start_time))
-
-
-	 
-	#file.write('Hello World')
-	 
 	file.close() 
-
-
-
 
 
 if __name__ == "__main__":
     cqf = int(sys.argv[1])
-    start_date = sys.argv[2]
-    end_date = sys.argv[3]
-    roastme(cqf, start_date, end_date)
+    roastme(cqf)
