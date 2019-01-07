dabc415d7b55314b362e1840a9d00959aebee947
Marty Corbett <corbe054@umn.edu>
2019-01-06 18:19:44 -0600

    update start.py to handle arguments

--- start.py
+++ start.py
@@ -1,55 +1,74 @@
 import praw
 from settings_secret import *
 import timeit
+import time
+import sys
 
 
-start_time = timeit.default_timer()
-reddit = praw.Reddit(client_id=client_id,
-                     client_secret=client_secret,
-                     user_agent=user_agent)
+def roastme(cqf, start_date, end_date):
+	start_time = timeit.default_timer()
+	reddit = praw.Reddit(client_id=client_id,
+	                     client_secret=client_secret,
+	                     user_agent=user_agent)
 
-#print(reddit.read_only)  # Output: True
+	#print(reddit.read_only)  # Output: True
 
-#total = 1
-rank = []
+	#total = 1
+	rank = []
 
-api_time = timeit.default_timer()
-print('begin')
+	api_time = timeit.default_timer()
+	print('begin')
+	print(cqf)
+	print(start_date)
+	print(end_date)
 
-roast = reddit.subreddit('roastme').hot(limit=10)
-print(timeit.default_timer() - api_time)
+	roast = reddit.subreddit('roastme').hot(limit=100)
+	print(timeit.default_timer() - api_time)
 
-for submission in roast:
-	for_time = timeit.default_timer()
-	submission.comments.replace_more(limit=0)
-	#top_level_comments = list(submission.comments)
-	print('top level comments: {}'.format(len(submission.comments)))
-	for comment in submission.comments:
-		#if comment
-		#body = comment.body
-		#if body != '[removed]' and comment.score >= 1000:
-		if comment.score >= 1000:
-			rank.append([comment.score, comment.body])
-			#print('{}: {}'.format(comment.score, body))
-			#total += 1
+	for submission in roast:
+		if submission.score >= 1000:
+			for_time = timeit.default_timer()
+			submission.comments.replace_more(limit=0)
+			#top_level_comments = list(submission.comments)
+			print('top level comments: {}'.format(len(submission.comments)))
+			for comment in submission.comments:
+				#if comment
+				#body = comment.body
+				#if body != '[removed]' and comment.score >= 1000:
+				if comment.score >= 1000:
+					rank.append([comment.score, comment.body])
+					#print('{}: {}'.format(comment.score, body))
+					#total += 1
 
-	print(timeit.default_timer() - for_time)
-    #print('{}: {}'.format(index + 1, submission.title))
+		print(timeit.default_timer() - for_time)
+	    #print('{}: {}'.format(index + 1, submission.title))
 
-rank = sorted(rank)
-rank.reverse()
+	rank = sorted(rank)
+	rank.reverse()
 
-file = open('C:\\Users\\Marty\\Desktop\\testfile.txt','w') 
+	datetime = time.strftime("%Y%m%d%H%M%S")
 
-for index, r in enumerate(rank):
-	#print('{}: {}'.format(*r[0], *r[1]))
-	#print(*r)
-	file.write('{}: {}\n'.format(index, r[1]))
+	file = open('{}-{}-{}-{}.txt'.format(filepath, cqf, start_date.replace('/', '-'), end_date.replace('/', '-')), 'w', encoding="utf-8")
 
-print('Elapsed Time: {} seconds'.format(timeit.default_timer() - start_time))
+	for index, r in enumerate(rank):
+		#print('{}: {}'.format(*r[0], *r[1]))
+		#print(*r)
+		file.write('{}: {}\n'.format(index, r[1]))
+
+	print('Elapsed Time: {} seconds'.format(timeit.default_timer() - start_time))
 
 
- 
-#file.write('Hello World')
- 
-file.close() 
+	 
+	#file.write('Hello World')
+	 
+	file.close() 
+
+
+
+
+
+if __name__ == "__main__":
+    cqf = int(sys.argv[1])
+    start_date = sys.argv[2]
+    end_date = sys.argv[3]
+    roastme(cqf, start_date, end_date)
